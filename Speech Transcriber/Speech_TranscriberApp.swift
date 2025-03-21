bdc918a08ce98e5a2bf175b5d05e0f15413192ef
jcleetus <joshua.cleetus@gmail.com>
2025-03-21 11:50:45 -0400

    Initial Commit

---
+++ Speech Transcriber/Speech_TranscriberApp.swift
@@ -0,0 +1,17 @@
+//
+//  Speech_TranscriberApp.swift
+//  Speech Transcriber
+//
+//  Created by Joshua Cleetus on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Speech_TranscriberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Speech TranscriberTests/Speech_TranscriberTests.swift
@@ -0,0 +1,17 @@
+//
+//  Speech_TranscriberTests.swift
+//  Speech TranscriberTests
+//
+//  Created by Joshua Cleetus on 3/21/25.
+//
+
+import Testing
+@testable import Speech_Transcriber
+
+struct Speech_TranscriberTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
